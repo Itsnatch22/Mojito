95c9e2449f6ebf293ab9e8d0c681170296be2084
Itsnatch <kamaumark996@gmail.com>
2025-06-21 00:28:37 +0300

    Video

--- 3d/src/components/Hero.jsx
+++ 3d/src/components/Hero.jsx
@@ -1,74 +1,127 @@
-import React from 'react'
-import { useGSAP } from '@gsap/react'
-import { SplitText } from 'gsap/all'
-import gsap from 'gsap'
-gsap.registerPlugin(SplitText)
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
-    useGSAP(() => {
-        const heroSplit = new SplitText('.title', {
-            type: 'chars, words'
-        });
-        const paragraphSplit = new SplitText('.subtitle', {
-            type: 'lines'
-        });
+ const videoRef = useRef();
+ 
+ const isMobile = useMediaQuery({ maxWidth: 767 });
+ 
+ useGSAP(() => {
+	const heroSplit = new SplitText(".title", {
+	 type: "chars, words",
+	});
+	
+	const paragraphSplit = new SplitText(".subtitle", {
+	 type: "lines",
+	});
+	
+	// Apply text-gradient class once before animating
+	heroSplit.chars.forEach((char) => char.classList.add("text-gradient"));
+	
+	gsap.from(heroSplit.chars, {
+	 yPercent: 100,
+	 duration: 1.8,
+	 ease: "expo.out",
+	 stagger: 0.06,
+	});
+	
+	gsap.from(paragraphSplit.lines, {
+	 opacity: 0,
+	 yPercent: 100,
+	 duration: 1.8,
+	 ease: "expo.out",
+	 stagger: 0.06,
+	 delay: 1,
+	});
+	
+	gsap
+	.timeline({
+	 scrollTrigger: {
+		trigger: "#hero",
+		start: "top top",
+		end: "bottom top",
+		scrub: true,
+	 },
+	})
+	.to(".right-leaf", { y: 200 }, 0)
+	.to(".left-leaf", { y: -200 }, 0)
+	.to(".arrow", { y: 100 }, 0);
+	
+	const startValue = isMobile ? "top 50%" : "center 60%";
+	const endValue = isMobile ? "120% top" : "bottom top";
+	
+	let tl = gsap.timeline({
+	 scrollTrigger: {
+		trigger: "video",
+		start: startValue,
+		end: endValue,
+		scrub: true,
+		pin: true,
+	 },
+	});
+	
+	videoRef.current.onloadedmetadata = () => {
+	 tl.to(videoRef.current, {
+		currentTime: videoRef.current.duration,
+	 });
+	};
+ }, []);
+ 
+ return (
+	<>
+	 <section id="hero" className="noisy">
+		<h1 className="title">Alchemist</h1>
+		
+		<img
+		 src="/images/hero-left-leaf.png"
+		 alt="left-leaf"
+		 className="left-leaf"
+		/>
+		<img
+		 src="/images/hero-right-leaf.png"
+		 alt="right-leaf"
+		 className="right-leaf"
+		/>
+		
+		<div className="body">
+		 <img src="/images/arrow.png" alt="arrow" className="arrow" /> 
+		 
+		 <div className="content">
+			<div className="space-y-5 hidden md:block">
+			 <p>Cool. Crisp. Classic.</p>
+			 <p className="subtitle">
+				Sip the essence of sophistication with our
+                handcrafted cocktails, <br /> where every sip is a journey through
+                the art of mixology.
+			 </p>
+			</div>
+			
+			<div className="view-cocktails">
+			 <p className="subtitle">
+				Every cocktail on our menu is a blend of premium ingredients,
+				creative flair, and timeless recipes — designed to delight your
+				senses.
+			 </p>
+			 <a href="#cocktails">View cocktails</a>
+			</div>
+		 </div>
+		</div>
+	 </section>
+	 
+	 <div className="video absolute inset-0">
+		<video
+		 ref={videoRef}
+		 muted
+		 playsInline
+		 preload="auto"
+		 src="/videos/output.mp4"
+		/>
+	 </div>
+	</>
+ );
+};
 
-        heroSplit.chars.forEach((char) => char.classList.add('text-gradient'));
-
-        gsap.from(heroSplit.chars, {
-            yPercent: 100,
-            duration: 1.8,
-            ease: 'expo.out',
-            stagger: 0.05,
-        })
-        gsap.from(paragraphSplit.lines, {
-            yPercent: 100,
-            opacity: 0,
-            duration: 1.8,
-            ease: 'expo.out',
-            stagger: 0.05,
-            delay: 1,
-        });
-
-        gsap.timeline({
-            scrollTrigger: {
-                trigger: '#hero',
-                start: 'top top',
-                end: '+=1000',
-                scrub: true,
-            }
-        })
-        .to('.right-leaf', {y: 200}, 0)
-        .to('.left-leaf', {y: -200}, 0)
-    }, []);
-  return (
-    <>
-    <section id='hero' className='noisy'>
-        <h1 className="title">Alchemist</h1>
-
-        <img src="/images/hero-left-leaf.png" alt="left-leaf" className='left-leaf' />
-        <img src="/images/hero-right-leaf.png" alt="right-leaf" className='right-leaf' />
-
-        <div className="body">
-            <div className="content">
-                <div className="space-y-5 hidden md:block">
-                    <p>Cool. Crisp. Classic</p>
-                    <p className="subtitle">
-                        Sip the essence of nature with <br />our artisanal alchemy, where every pour is a journey through the forest's heart.
-                    </p>
-                </div>
-
-                <div className="view-cocktails">
-                    <p className="subtitle">
-                        Every cocktail is a masterpiece, crafted with the finest ingredients and a touch of magic. <br />
-                    </p>
-                    <a href="#cocktails">View Cocktails</a>
-                </div>
-            </div>
-        </div>
-    </section>
-    </>
-  )
-}
-
-export default Hero
+export default Hero;
